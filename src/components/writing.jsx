155533601791a7ef49c0348523dc96d7bf98077e
tushar <prajapatitushar789@gmail.com>
2022-10-05 15:11:24 +0530

    changes

--- src/components/writing.jsx
+++ src/components/writing.jsx
@@ -33,9 +33,9 @@ function Writing() {
   return (
     <section
       ref={ref}
-      className={`md:flex scroll-area md:py-8 lg:py-2 relative overflow-y-scroll overflow-x-hidden  px-10 bg-blue-100 py-8 md:bg-white`}
+      className={`md:flex scroll-area md:py-8 lg:py-2 relative overflow-y-scroll lg:max-h-screen overflow-x-hidden  px-10 bg-blue-100 py-8 md:bg-white`}
     >
-      <div className="lg:h-[100vh] lg:w-[25vw] md:basis-[45%] lg:basis-[30%]">
+      <div className="lg:h-full lg:w-[25vw] md:basis-[45%] lg:basis-[30%]">
         <img
           className="h-48 mx-auto md:hidden w-36"
           src={sandeep}
@@ -54,14 +54,20 @@ function Writing() {
       <div className="text-[12px] md:basis-[70%] lg:leading-6 line tracking-wider md:tracking-widest md:border-t-3 lg:text-sm xl:text-base relative md:rounded-bl-[140px] md:text-xs lg:sm md:box md:border-t-4  md:border-blue-400 md:px-14 md:py-14 px-6">
         Hi, This is Sandeep Gupta, the foremost
         <button
-          onClick={() => setGmat(true)}
+          onClick={() => {
+            setGmat(true);
+            setGre(false);
+          }}
           className="py-1 px-2 mr-1 ml-1 border-2 font-semibold border-black text-white bg-orange-500 rounded-md"
         >
           GMAT
         </button>
         /
         <button
-          onClick={() => setGre(true)}
+          onClick={() => {
+            setGre(true);
+            setGmat(false);
+          }}
           className=" py-1 ml-1 px-2 border-2 font-semibold border-black text-white bg-orange-500 rounded-md"
         >
           GRE
@@ -77,7 +83,7 @@ function Writing() {
         multiple times but that, by using the same techniques that I used
         (OCTAVE, CLESSSP, PRIME, ACT, ANT, ACED, LINGO, and TRAPS), my students
         have been able to get similar successes on more than 20,000 occasions.
-        <div className=" hidden md:block md:top-6 md:text-xl lg:top-32  left-0 relative cursive text-center lg:text-3xl">
+        <div className=" hidden md:block md:top-6 md:text-xl lg:top-16  left-0 relative cursive text-center lg:text-3xl">
           {" "}
           <Typewriter
             options={{
